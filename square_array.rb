dd15b8cb2584eb4d3778e30b88ce164d816e3115
Melissa Holt <melissa.holt21@gmail.com>
2019-11-11 02:10:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 new_array = []
 
 def square_array(array)
-  array.each do |squared_number|
-    puts |squared_**2| << squared_array 
+  array.each do |squared_number|**2
+    puts new_array << 
   end
 return new_array
 end
